fc804413f2e1482616e41c82ba74294abfcd2bcc
Vin <vincentwei@healife.com>
2025-06-10 15:19:55 +0800

    init

---
+++ src/main/java/com/john/Application.java
@@ -0,0 +1,17 @@
+package com.john;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wtwei
+ * <p>create time: 2025/6/10 15:11</p>
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class, args);
+    }
+}
